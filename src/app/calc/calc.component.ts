bb445528cbcfd9b8958d3d66f355d47d2edde1fb
front42 <altaifront@gmail.com>
2025-04-09 15:20:38 +0300

    feat: add calculator functionality

--- src/app/calc/calc.component.ts
+++ src/app/calc/calc.component.ts
@@ -10,35 +10,17 @@ import { NgFor } from '@angular/common';
   imports: [NgFor],
 })
 export class CalcComponent {
-  public numpadButtons: string[] = [
-    'AC',
-    '+/-',
-    '%',
-    '÷',
-    '7',
-    '8',
-    '9',
-    '×',
-    '4',
-    '5',
-    '6',
-    '-',
-    '1',
-    '2',
-    '3',
-    '+',
-    '0',
-    '.',
-    '=',
-  ];
+  public numpad: string[] = ['AC', '+/-', '%', '÷', '7', '8', '9', '×', '4', '5', '6', '–', '1', '2', '3', '+', '0', '.', '='];
   public screenValue: string = '0';
   public screenValueFontSize: string = '68px';
 
-  constructor(private router: Router) {
-    console.log(typeof this.screenValue);
-  }
+  public firstArg: string = '';
+  public secondArg: string = '';
+  public operator: string = '';
 
-  public goHome() {
+  constructor(private router: Router) {}
+
+  public goHome(): void {
     this.router.navigate(['/']);
   }
 
@@ -47,7 +29,7 @@ export class CalcComponent {
 
     switch (value) {
       case 'AC':
-        this.screenValue = '0';
+        this.reset();
         break;
       case '+/-':
         if (this.screenValue) this.screenValue = String(+this.screenValue * -1);
@@ -57,22 +39,75 @@ export class CalcComponent {
         break;
       case '÷':
       case '×':
-      case '-':
+      case '–':
       case '+':
       case '=':
+        if (this.operator && !this.secondArg) {
+          this.operator = value;
+          console.log('Operator: ' + this.operator);
+          return;
+        }
+        if (this.firstArg && this.operator) {
+          this.secondArg = this.screenValue;
+          console.log('second: ' + this.secondArg);
+          this.getResult();
+        }
+        if (!this.firstArg) {
+          this.firstArg = this.screenValue;
+          console.log('first: ' + this.firstArg);
+        }
+        this.operator = value;
+        console.log('Operator: ' + this.operator);
         break;
       case '.':
         if (!isDecimalPart) this.screenValue += '.';
         break;
       default:
-        if (!isNaN(+value) && this.screenValue != '0') {
+        if (isNaN(+this.screenValue)) return;
+        if (this.screenValue !== '0') {
           this.screenValue += value;
-          if (this.screenValue.length > 14)
-            this.screenValue = 'Hey, stop it please!';
+          if (this.screenValue.length > 14) this.screenValue = 'Hey, stop it please!';
         } else {
           this.screenValue = value;
         }
+        if (this.firstArg && this.operator && !this.secondArg) {
+          this.secondArg = this.screenValue = value;
+        }
     }
     this.screenValueFontSize = this.screenValue.length > 7 ? '34px' : '68px';
+  }
+
+  private getResult(): void {
+    let result: number | string = 0;
+    const firstArg = parseFloat(this.firstArg);
+    const secondArg = parseFloat(this.secondArg);
+
+    switch (this.operator) {
+      case '÷':
+        if (secondArg) {
+          result = firstArg / secondArg;
+        } else {
+          result = `Can't divide by zero`;
+        }
+        break;
+      case '×':
+        result = firstArg * secondArg;
+        break;
+      case '–':
+        result = firstArg - secondArg;
+        break;
+      case '+':
+        result = firstArg + secondArg;
+        break;
+    }
+    this.reset();
+    this.screenValue = String(result);
+  }
+
+  private reset(): void {
+    this.screenValue = '0';
+    this.firstArg = '';
+    this.secondArg = '';
+    this.operator = '';
   }
 }
