66a077de1708ecf3fc5f4c8a167cb85412e977fe
front42 <altaifront@gmail.com>
2025-06-19 10:38:33 +0300

    feat: add localstorage database

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -7,7 +7,7 @@ export interface ITodo {
 
 @Injectable({ providedIn: 'root' })
 export class DataService {
-  private notes: string[] = [
+  private defaultNotes: string[] = [
     `Let's make the first note!`,
     'Between the 1st and the 2nd - just a second!',
     'What did they say about the Trinity?',
@@ -16,7 +16,7 @@ export class DataService {
     'Hidden note.',
   ];
 
-  private todos: ITodo[] = [
+  private defaultTodos: ITodo[] = [
     { description: 'eat', done: false },
     { description: 'sleep', done: true },
     { description: 'play', done: false },
@@ -27,35 +27,61 @@ export class DataService {
 
   constructor() {}
 
+  public updateNotes(notes: string[]): void {
+    localStorage.setItem('notes', JSON.stringify(notes));
+  }
+
   public getNotes(): string[] {
-    return this.notes;
+    return JSON.parse(localStorage.getItem('notes') as string) || this.defaultNotes;
   }
 
   public addNote(note: string): void {
-    this.notes.unshift(note);
+    const notes = this.getNotes();
+    notes.unshift(note);
+    this.updateNotes(notes);
   }
 
   public editNote(index: number, editedNote: string): void {
-    this.notes.splice(index, 1, editedNote);
+    const notes = this.getNotes();
+    notes.splice(index, 1, editedNote);
+    this.updateNotes(notes);
   }
 
   public removeNote(index: number): void {
-    this.notes.splice(index, 1);
+    const notes = this.getNotes();
+    notes.splice(index, 1);
+    this.updateNotes(notes);
+  }
+
+  public updateTodos(todos: ITodo[]): void {
+    localStorage.setItem('todos', JSON.stringify(todos));
   }
 
   public getTodos(): ITodo[] {
-    return this.todos;
+    return JSON.parse(localStorage.getItem('todos') as string) || this.defaultTodos;
   }
 
   public addTodo(todo: ITodo): void {
-    this.todos.unshift(todo);
+    const todos = this.getTodos();
+    todos.unshift(todo);
+    this.updateTodos(todos);
   }
 
   public editTodo(index: number, newDescription: string): void {
-    this.todos[index].description = newDescription;
+    const todos = this.getTodos();
+    todos[index].description = newDescription;
+    this.updateTodos(todos);
   }
 
   public removeTodo(index: number): void {
-    this.todos.splice(index, 1);
+    const todos = this.getTodos();
+    todos.splice(index, 1);
+    this.updateTodos(todos);
+  }
+
+  public doneChange(index: number) {
+    const todos = this.getTodos();
+    todos[index].done = !todos[index].done;
+    this.updateTodos(todos);
   }
 }
